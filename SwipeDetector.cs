4dec2478f9468227d1d515eb6b3f52ce0b947148
abhinav <abhinav.malkoochi@gmail.com>
2023-04-22 00:32:38 -0500

    added position instantiation

--- SwipeDetector.cs
+++ SwipeDetector.cs
@@ -17,7 +17,12 @@ public class SwipeDetector : MonoBehaviour
 	private float playerHeight;
 	private float angle;
     SpriteRenderer sRenderer;
-
+	private float counter=0;
+	private GameObject o;
+	private float radians;
+	private float xPos;
+	private float yPos;
+	
 	void Awake()
     {
         width = (float)Screen.width / 2.0f;
@@ -55,43 +60,38 @@ public class SwipeDetector : MonoBehaviour
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
+				counter++;
 				Debug.Log("Touch Ended"+endTouchPosition.ToString());
 				endTouchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-				//angle = Vector2.Angle(startTouchPosition,endTouchPosition);
 				float yDifference =  endTouchPosition.y-startTouchPosition.y;
 				float xDifference = endTouchPosition.x-startTouchPosition.x;
 				Debug.Log("YDifference: "+yDifference+" xDifference: "+xDifference);
-				angle = Mathf.Atan2((endTouchPosition.y-startTouchPosition.y),(endTouchPosition.x-startTouchPosition.x))*Mathf.Rad2Deg;
-				Debug.Log("Angle"+angle);
+				angle = Mathf.Atan2((yDifference),(xDifference))*Mathf.Rad2Deg;
+				radians = Mathf.Atan2((yDifference),(xDifference));
 				if(angle<0){
 					angle=0;
 				}else if(angle>180){
 					angle=180;
 				}
-				GameObject o = Instantiate(gb, new Vector2(endTouchPosition.x,endTouchPosition.y), Quaternion.identity);
-				o.transform.rotation = Quaternion.Euler(Vector3.forward * (angle-90));				//Quaternion upright = Quaternion.Euler(0f,0f,angle);
+				if(counter==0){
+					o =	(GameObject)Instantiate(gb, new Vector2(gameObject.transform.position.x,gameObject.transform.position.y), Quaternion.identity);
+					xPos=gameObject.transform.position.x;
+					yPos=gameObject.transform.position.y;
+					o=new GameObject();
 
-				//Instantiate(gb, new Vector2(endTouchPosition.x,endTouchPosition.y), upright);
-
-			/*
-			endTouchPosition.x=(endTouchPosition.x-width)/width;
-			endTouchPosition.y=(endTouchPosition.y-height)/height;
-			
-			angle = (float)Mathf.Acos(currentPosition.x);
-			Debug.Log("Qauternion Angles"+upright.ToString());
-			if(currentPosition.y<0){
-				angle=-angle;
-			}			
-			Vector2 Distance = endTouchPosition - startTouchPosition;
-			float sign = (endTouchPosition.y < startTouchPosition.y)? -1.0f : 1.0f;
-         	float  pos=Vector2.Angle(Vector2.right, Distance) * sign;
-			Quaternion upright = Quaternion.Euler(pos*(Mathf.PI/180),0f,0f);
-			//Instantiate(gb, new Vector2(endTouchPosition.x,endTouchPosition.y+(playerHeight/2)), upright);	
-			Instantiate(gb, new Vector2(.1f,-4f+(playerHeight/2)), upright);
-            stopTouch = false;
-
-			Debug.Log("EndPos"+endTouchPosition.ToString());
-		*/
+				}else if(counter>0 && angle<90){
+					 o = (GameObject)Instantiate(gb, new Vector2(Mathf.Cos(radians)*(playerHeight/2)+xPos,Mathf.Sin(radians)*(playerHeight/2)+yPos), Quaternion.identity);
+					 xPos=Mathf.Cos(radians)*(playerHeight/2)+o.transform.position.x;
+					 yPos=Mathf.Sin(radians)*(playerHeight/2)+o.transform.position.y;
+					 o=new GameObject();
+				}
+				else if(counter>0 && angle>90){
+					 o = (GameObject)Instantiate(gb, new Vector2(Mathf.Cos(radians)*(playerHeight/2)-xPos,Mathf.Sin(radians)*(playerHeight/2)+yPos), Quaternion.identity);
+					 xPos=Mathf.Cos(radians)*(playerHeight/2)-o.transform.position.x;
+					 yPos=Mathf.Sin(radians)*(playerHeight/2)+o.transform.position.y;
+					 o=new GameObject();
+				}
+				o.transform.rotation = Quaternion.Euler(Vector3.forward * (angle-90));
 
         }
 
